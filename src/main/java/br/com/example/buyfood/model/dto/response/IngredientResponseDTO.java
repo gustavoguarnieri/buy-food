0dd7f20579bb0ae549f900ad41b6f3be051f256b
Gustavo <gustavo.guarnieri@gmail.com>
2021-02-23 00:02:34 -0300

    added response id for ingredient

--- src/main/java/br/com/example/buyfood/model/dto/response/IngredientResponseDTO.java
+++ src/main/java/br/com/example/buyfood/model/dto/response/IngredientResponseDTO.java
@@ -3,12 +3,11 @@ package br.com.example.buyfood.model.dto.response;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.NotBlank;
-
 @Getter
 @Setter
 public class IngredientResponseDTO {
 
+    private Long id;
     private String ingredient;
     private String portion;
     private int status;
