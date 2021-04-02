1b5606c21ce21119b50f5328b9c711671a190469
Gustavo <gustavo.guarnieri@gmail.com>
2021-04-02 00:39:53 -0300

    adjusted delivery tax swagger documentation

--- src/main/java/br/com/example/buyfood/controller/EstablishmentDeliveryTaxController.java
+++ src/main/java/br/com/example/buyfood/controller/EstablishmentDeliveryTaxController.java
@@ -94,7 +94,8 @@ public class EstablishmentDeliveryTaxController {
     @ResponseStatus(HttpStatus.CREATED)
     @ApiOperation(value = "Create a new delivery tax")
     @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Created delivery tax", response = EstablishmentDeliveryTaxResponseDTO.class),
+            @ApiResponse(code = 201, message = "Created delivery tax",
+                    response = EstablishmentDeliveryTaxResponseDTO.class),
             @ApiResponse(code = 401, message = "You are unauthorized to access this resource"),
             @ApiResponse(code = 403, message = "You do not have permission to access this resource"),
             @ApiResponse(code = 500, message = "An exception was thrown"),
