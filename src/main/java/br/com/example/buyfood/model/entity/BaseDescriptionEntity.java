c35bb6ff65bb896227b02407a37db11325413d67
Gustavo <gustavo.guarnieri@gmail.com>
2021-09-16 01:10:07 -0300

    created MappedSuperclass BaseDescriptionEntity

---
+++ src/main/java/br/com/example/buyfood/model/entity/BaseDescriptionEntity.java
@@ -0,0 +1,37 @@
+package br.com.example.buyfood.model.entity;
+
+import br.com.example.buyfood.enums.RegisterStatus;
+import br.com.example.buyfood.model.embeddable.Audit;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseDescriptionEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int status = RegisterStatus.ENABLED.getValue();
+
+    @Embedded
+    private Audit audit = new Audit();
+}
